96ee191bf4c0450619ba740b478df6b711a6e822
Kirikmelet <troy.dwijanto2@gmail.com>
2019-12-16 17:31:35 +0700

    Second Initial Commit

---
+++ animelyricsdl.py
@@ -0,0 +1,32 @@
+import animelyrics
+
+
+"""
+CONFIG BELOW THIS TEXT
+"""
+CFG_TITLE = "False"
+CFG_LANG = "en"
+"""
+CONFIG ABOVE THIS TEXT
+"""
+
+Q1_NAME = str(input("What songname? ~$ "))
+Q1_ARTIST = str(input("What Artist? ~$ "))
+
+if Q1_ARTIST is not "":
+    Q1_ARTIST = ""
+    Q1 = str(Q1_ARTIST + " _ " + Q1_NAME)
+else:
+    Q1 = str(Q1_NAME)
+
+try:
+    S_LYRICS = animelyrics.search_lyrics(Q1, lang=CFG_LANG, show_title=CFG_TITLE)
+except animelyrics.NoLyricsFound:
+    print("FAIL!: NOT FOUND!")
+except animelyrics.MissingTranslatedLyrics:
+    print("FAIL!: LANGUAGE PERIMETER ( {} ) MISSING! ")
+
+print(S_LYRICS)
+with open('{}.txt'.format(Q1), 'w') as S_TEXT:
+    S_TEXT.write(S_LYRICS)
+    S_TEXT.close()
