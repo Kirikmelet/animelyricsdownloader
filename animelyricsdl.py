90c16ec87186f0ea69ef1418e6310c411b791b66
Kirikmelet <troy.dwijanto2@gmail.com>
2019-12-17 17:45:46 +0700

    Scripts Update
    added some stuff

--- animelyricsdl.py
+++ animelyricsdl.py
@@ -13,7 +13,7 @@ CONFIG ABOVE THIS TEXT
 Q1_NAME = str(input("What songname? ~$ "))
 Q1_ARTIST = str(input("What Artist? ~$ "))
 
-if Q1_ARTIST is not "":
+if Q1_ARTIST != "":
     Q1_ARTIST = ""
     Q1 = str(Q1_ARTIST + " _ " + Q1_NAME)
 else:
@@ -24,8 +24,7 @@ try:
 except animelyrics.NoLyricsFound:
     print("FAIL!: NOT FOUND!")
 except animelyrics.MissingTranslatedLyrics:
-    print("FAIL!: LANGUAGE PERIMETER ( {} ) MISSING! ")
-
+    S_LYRICS = animelyrics.search_lyrics(Q1, lang="jp", show_title=CFG_TITLE)
 print(S_LYRICS)
 with open('{}.txt'.format(Q1), 'w') as S_TEXT:
     S_TEXT.write(S_LYRICS)
